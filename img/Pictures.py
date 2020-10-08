3a3694718326fad5f72c6f9ba00dcb48d2cc664d
Martin Brajer <martin.brajer@seznam.cz>
2020-10-08 10:10:10 +0200

    Doublepage 9, remaining "\dot" to "\ldot".

--- img/Pictures.py
+++ img/Pictures.py
@@ -4,6 +4,7 @@ Scripts to generate pictures in lecture-notes-mathematical-analysis LaTeX projec
 """
 
 # --IMPORTS---
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 # import pandas as pd
@@ -15,7 +16,34 @@ import numpy as np
 
 
 def main():
-    typesOfDiscontinuity()
+    limitMultiplication()
+    # patrolmen()
+    # typesOfDiscontinuity()
+
+
+def limitMultiplication():
+    """ Example 2.2. """
+    x = np.linspace(-1, 1, 1000)
+    y = x * np.sin(1/x)
+
+    plt.plot(x, y, 'r')
+    plt.show()
+
+
+def patrolmen():
+    """ Theorem 2.4. """
+    x = np.linspace(-1, 1, 1000)
+    f = x**2
+    g = -f
+    h = f * np.sin(20*1/x)
+
+    size = matplotlib.rcParams['figure.figsize']
+    size[0] *= 2
+    plt.figure(figsize=size)
+    plt.plot(x, f, 'k')
+    plt.plot(x, g, 'r')
+    plt.plot(x, h, 'b')
+    plt.show()
 
 
 def typesOfDiscontinuity():
